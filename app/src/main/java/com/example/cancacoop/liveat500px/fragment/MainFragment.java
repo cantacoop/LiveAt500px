d3b635a28004e73b4d7f889ddb0fd2277305d675
Cantacoop <cantacoop@gmail.com>
2018-05-10 15:45:15 +0700

    Improve Pull to Refresh

--- app/src/main/java/com/example/cancacoop/liveat500px/fragment/MainFragment.java
+++ app/src/main/java/com/example/cancacoop/liveat500px/fragment/MainFragment.java
@@ -110,6 +110,10 @@ public class MainFragment extends Fragment {
             if (response.isSuccessful()) {
                 PhotoItemCollectionDao dao = response.body();
 
+                int firstVisiblePosition = listView.getFirstVisiblePosition();
+                View c = listView.getChildAt(0);
+                int top = c  == null ? 0 : c.getTop();
+
                 if (mode == MODE_RELOAD_NEWER)
                     photoListManager.insertDaoAtTopPosition(dao);
                 else
@@ -117,6 +121,17 @@ public class MainFragment extends Fragment {
 
                 listAdapter.setDao(photoListManager.getDao());
                 listAdapter.notifyDataSetChanged();
+
+                if (mode == MODE_RELOAD_NEWER) {
+                    // Maintain Scroll Position
+                    int additionalSize =
+                            (dao != null && dao.getData() != null) ? dao.getData().size() : 0;
+                    listAdapter.increaseLastPosition(additionalSize);
+                    listView.setSelectionFromTop(firstVisiblePosition + additionalSize,
+                            top);
+                } else {
+
+                }
 
                 Toast.makeText(Contextor.getInstance().getContext(),
                         "Load Completed",
